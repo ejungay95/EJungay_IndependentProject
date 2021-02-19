af2bc4f431d1a9742b6dcf610df25bed085d4a62
ejungay95 <elijah.jungay@gmail.com>
2021-02-19 12:39:26 -0800

    Got basic user movement and created game map

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+  [SerializeField] float playerSpeed = 10.0f;
+  [SerializeField] float turnSpeed = 60.0f;
+  [SerializeField] float cannonRotateSpeed = 250f;
+
+  public GameObject cannon;
+  public GameObject newCenterOfMass;
+  public int foodAmmo = 10;
+
+  private float horizontalInput;
+  private float verticalInput;
+  
+  private int cannonRotateDirection = 1;
+
+  // Start is called before the first frame update
+  void Start() {
+    GetComponent<Rigidbody>().centerOfMass = newCenterOfMass.transform.localPosition;
+  }
+
+  // Update is called once per frame
+  void Update() {
+    // ---- Input Stuff ----
+    // Note: unmapped arrow keys for use of cannon rotation
+    horizontalInput = Input.GetAxis("Horizontal");
+    verticalInput = Input.GetAxis("Vertical");
+
+    // Note: unmapped "Jump" to utilize use for spacebar
+    if(Input.GetButtonDown("Fire1")) {
+      Debug.Log("Pew pew");
+      if(foodAmmo > 0) {
+        foodAmmo -= 1;
+      }     
+    }
+
+    // Cannon rotation
+    if(Input.GetKey(KeyCode.LeftArrow)) {
+      cannon.transform.Rotate(Vector3.up * cannonRotateSpeed * -cannonRotateDirection * Time.deltaTime);
+    } else if(Input.GetKey(KeyCode.RightArrow)) {
+      cannon.transform.Rotate(Vector3.up * cannonRotateSpeed * cannonRotateDirection * Time.deltaTime);
+    }
+
+    // Move and rotate the player
+    transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime * verticalInput);
+    transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);  
+  }
+}
---
+++ Assets/Scripts/RefillAmmo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RefillAmmo : MonoBehaviour {
+  private const int MAX_FOOD_AMMO = 10;
+  private PlayerController playerController;
+
+  // Start is called before the first frame update
+  void Start() {
+    playerController = gameObject.GetComponent<PlayerController>();
+  }
+
+  private void OnTriggerEnter(Collider other) {
+    if(other.tag == "RefillZone") {
+      if(playerController.foodAmmo != MAX_FOOD_AMMO) {
+        playerController.foodAmmo = MAX_FOOD_AMMO;
+      }
+    }
+  }
+}
